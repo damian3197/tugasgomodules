609cc4098a9c0fd3238f076fce9c1d3b3a5b0bda
damian3197 <damiansitanggang@gmail.com>
2022-03-13 13:52:20 +0700

    add function welcome verifikasi dan dendabuku

--- modul.go
+++ modul.go
@@ -1,3 +1,19 @@
 package modul
 
 import "fmt"
+
+// function
+func Welcome() {
+	fmt.Println("Perpustakaan Kota")
+}
+
+// function sebagai parameter
+func Verifikasi(namaDepan string, namaBelakang string) {
+	fmt.Println("Verifikasi", namaDepan, namaBelakang, "Selamat Datang Di Perpustakaan")
+}
+
+// funtion dengan return value
+func Dendabuku(hargaDenda, LamaPeminjaman int) int{
+	denda := hargaDenda * LamaPeminjaman
+	return denda
+	}
